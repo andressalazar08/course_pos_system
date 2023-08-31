334ea8ce2e4f58e240ce93e116d4a52246d1d35e
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-08-31 11:24:45 -0500

    dotenv configured

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,13 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 
 
+dotenv.config({path:'src/config/.env'})
+
+
+const PORT = process.env.PORT
 
 
 const server = app.listen(4000, ()=>{
-    console.log(`server running`)
+    console.log(`server running ${PORT}`)
 })
