69db4b5fe2160d7ab7edcefb3182856b14319537
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-08-31 13:22:34 -0500

    simple api test

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,12 @@ const express = require('express');
 
 const app = express();
 
-
+//simple api test
+app.get('/', (req,res)=>{
+    res.json({
+        message:"POS api"
+    })
+})
 
 
 module.exports = app;
