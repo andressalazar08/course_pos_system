59b285604432f80be6a4115cfba00310fe20bf7d
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-09-01 12:03:02 -0500

    associations between Product and Category

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,6 +1,6 @@
 const sequelize = require('../config/database');
 const { DataTypes } = require('sequelize');
-
+const Category = require('./Category');
 
 const Product = sequelize.define('Product',{
     name:{
@@ -18,6 +18,10 @@ const Product = sequelize.define('Product',{
         type:DataTypes.INTEGER,
         allowNull:false
     }
-})
+});
 
+//Associations
+Category.hasMany(Product)
+Product.belongsTo(Category);
+//ON DELETE defaults to SET NULL and ON UPDATE defaults to CASCADE
 module.exports = Product;
