d200797fb9612c496054b3425c2814286777ce41
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-09-02 10:08:12 -0500

    user controller and route

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,84 @@
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const SECRET_KEY = process.env.SECRET_KEY;;//TODO:
+
+// Crea un nuevo usuario
+const createUser = async (req, res) => {
+  
+  // Valida los datos de la solicitud utilizando express-validator
+  const errors = validationResult(req);
+
+  // Si hay errores de validación, responde con un mensaje de error
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // Si no hay errores de validación, continúa con la lógica para crear un usuario
+
+
+  const { username, email, password } = req.body;
+
+  try {
+    // Verifica si el usuario ya existe
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'El usuario ya existe.' });
+    }
+
+    // Hashea la contraseña antes de almacenarla en la base de datos
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Crea el nuevo usuario
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Genera un token de autenticación
+    const token = jwt.sign({ userId: newUser.id }, SECRET_KEY, { expiresIn: '1h' });
+
+    res.status(201).json({ user: newUser, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al crear el usuario.' });
+  }
+};
+
+// Iniciar sesión de usuario
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Verifica si el usuario existe
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Credenciales incorrectas.' });
+    }
+
+    // Compara la contraseña ingresada con la almacenada en la base de datos
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Credenciales incorrectas.' });
+    }
+
+    // Genera un token de autenticación
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: '1h' });
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al iniciar sesión.' });
+  }
+};
+
+module.exports = {
+  createUser,
+  loginUser,
+};
---
+++ backend/src/routes/userRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { body } = require('express-validator');
+const { createUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+// Definir la regla de validación para la creación de un usuario
+//CREAR LA CARPETA middlewares y dejar esta función ahí
+const userValidationRules = [
+  body('username').trim().not().isEmpty().withMessage('El nombre de usuario es obligatorio.'),
+  body('email').trim().isEmail().withMessage('Introduce una dirección de correo electrónico válida.'),
+  body('password').trim().isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres.'),
+];
+
+// Ruta para crear un nuevo usuario
+router.post('/create', userValidationRules, createUser);
+
+module.exports = router;
