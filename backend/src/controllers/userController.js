b7ce929e2ae9c182fe004a1248c4d871907ccc7b
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-09-20 08:45:54 -0500

    register user with password hashed

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-
+const bcrypt = require('bcrypt');
 
 const registerUser = async(req, res)=>{
 
@@ -11,10 +11,19 @@ const registerUser = async(req, res)=>{
             return res.status(400).json({message:'User already exists, cannot create a user with this email'});
         }
 
+        //encrypt password before create
+        let hashedPassword;
+        try{
+            hashedPassword = await bcrypt.hash(password,10);
+        }catch(hashError){
+            return {message:'Internal error on password hashing'};
+        }
+
+
         const newUser = await User.create({
             name,
             email,
-            password,
+            password:hashedPassword,
             profile,
             imageUrl,
         });
