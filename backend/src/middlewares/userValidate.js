a713d9db5edce8d74e69c1721e925be3cad203b0
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-09-21 08:31:10 -0500

    email validation from middleware

--- backend/src/middlewares/userValidate.js
+++ backend/src/middlewares/userValidate.js
@@ -2,18 +2,19 @@ const { ClientError } = require('../utils/clientError');
 
 const newUserValidation = (req, res, next)=>{
 
-    const { name, password } = req.body;
+    const { name, password, email } = req.body;
+    
+    if(!name) throw new ClientError('Name cannot be empty', 400);
 
-    if(name){
-        if(password){
-            return next();
-        }else{
-            throw new ClientError('Password cannot be empty', 400);
-        }
-    }else{
-        throw new ClientError('Name cannot be empty', 400);
-    }
+    if(!password) throw new ClientError('Password cannot be empty', 400);
 
+    //validate email here
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const isValidEmail = emailRegex.test(email);
+
+    if(!isValidEmail) throw new ClientError('Email is not valid');
+
+    return next();
 };
 
 module.exports ={
