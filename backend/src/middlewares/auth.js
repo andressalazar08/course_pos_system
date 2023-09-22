44e29c958e374689b6ffbe7e23ca45582c67bb5c
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-09-22 12:41:36 -0500

    admin route get all users info

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,6 +1,8 @@
+const User = require('../models/User');
 const { ClientError } = require('../utils/clientError');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config({path:'src/config/.env'});
+const { catchAsyncErrors } = require('../middlewares/catchAsync');
 
 const isAuthenticatedUser = (req, res, next)=>{
     const { token } = req.cookies;
@@ -11,4 +13,15 @@ const isAuthenticatedUser = (req, res, next)=>{
     next();
 };
 
-module.exports = { isAuthenticatedUser };
+const authorizedProfile = catchAsyncErrors(async(req, res, next)=>{
+    req.user = await User.findByPk(req.user.userId);
+    if(req.user.profile!=='admin'){
+        throw new ClientError(`Profile [${req.user.profile}] is not allowed to access this resource`, 403);
+    }
+    next();
+});
+
+module.exports = { 
+    isAuthenticatedUser,
+    authorizedProfile,
+ };
