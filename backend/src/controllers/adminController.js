243e29a4f78c1c0d1f1eaa64f8b5c2bf36e3e3fd
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-09-25 17:19:43 -0500

    all users with keyword and isactive search and filter

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,15 +1,38 @@
 const User = require('../models/User');
 const { ClientError } = require('../utils/clientError');
 const { catchAsyncErrors } = require('../middlewares/catchAsync');
-
+const op = require('sequelize').Op;
 
 const allUsers = catchAsyncErrors(async(req, res, next)=>{
+
+    const { keyword, isActive, page, limit } = req.query;
+
+    const whereClause={};
+
+    if(keyword){
+        whereClause.name={
+            [op.like]:`%${keyword}%`
+        }
+    }
+
+    if(isActive){
+        whereClause.isActive=isActive
+    }
+    
+    //console.log(whereClause);
+    const offset = (page-1)*limit;
+
     const users = await User.findAll({
-        attributes:['name', 'email', 'profile', 'imageUrl', 'isActive']
+        attributes:['name', 'email', 'profile', 'imageUrl', 'isActive'],
+        where:whereClause,
+        limit:parseInt(limit),
+        offset:offset,
+
     });
 
     return res.status(200).json({
-        message:'All users retrieved',
+        message:'Filtered and paginated users retrieved',
+        count:users.length,
         users
     });
 });
